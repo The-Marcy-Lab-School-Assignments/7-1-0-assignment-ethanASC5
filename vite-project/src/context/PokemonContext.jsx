173b475908696e8876ac800a9ef8fbb77878302f
Marcy <marcy@Marcys-MBP-4.home>
2024-08-28 20:14:50 -0400

    Finished

--- vite-project/src/context/PokemonContext.jsx
+++ vite-project/src/context/PokemonContext.jsx
@@ -1,1 +1,7 @@
 // TODO: Create the context
+import { createContext } from "react"; // importing context from react
+
+const PokemonContext = createContext(); // creating context 
+
+
+export default PokemonContext; // exporting it 
